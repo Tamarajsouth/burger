87aadea52e3a9e4db99e5a1ce2f3461d19c3bb4a
tamara south <tjeansouth@gmail.com>
2020-05-08 12:15:34 -0400

    edit connection.js with jawsDB environment for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-let connection;
+var connection;
 //  jawsDBenvironment on heroku 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
